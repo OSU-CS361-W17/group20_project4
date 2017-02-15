325f35e6b9cb144d49e5829c002cb633b62f38ac
aoleson <alannah313@gmail.com>
2017-02-14 21:33:00 -0800

    Cleaning up in preparation for PR

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,7 +1,7 @@
 var gameModel;
 var currentChosenRow = 0;
 var currentChosenCol = 0;
-var scanFlag = 0;
+var scanFlag = 0;           //So that the scan function only fires on the scan option
 
 $( document ).ready(function() {
   // Handler for .ready() called.
@@ -38,55 +38,6 @@ function placeShip() {
 }
 
 
-
-
-/*function fire(){
- console.log($( "#colFire" ).val());
-   console.log($( "#rowFire" ).val());
-//var menuId = $( "ul.nav" ).first().attr( "id" );
-   var request = $.ajax({
-     url: "/fire/"+$( "#colFire" ).val()+"/"+$( "#rowFire" ).val(),
-     method: "post",
-     data: JSON.stringify(gameModel),
-     contentType: "application/json; charset=utf-8",
-     dataType: "json"
-   });
-
-   request.done(function( currModel ) {
-     displayGameState(currModel);
-     gameModel = currModel;
-
-   });
-
-   request.fail(function( jqXHR, textStatus ) {
-     alert( "Request failed: " + textStatus );
-   });
-
-}*/
-
-/*function scan(){
- console.log($( "#colScan" ).val());
-   console.log($( "#rowScan" ).val());
-//var menuId = $( "ul.nav" ).first().attr( "id" );
-   var request = $.ajax({
-     url: "/scan/"+$( "#colScan" ).val()+"/"+$( "#rowScan" ).val(),
-     method: "post",
-     data: JSON.stringify(gameModel),
-     contentType: "application/json; charset=utf-8",
-     dataType: "json"
-   });
-
-   request.done(function( currModel ) {
-     displayGameState(currModel);
-     gameModel = currModel;
-
-   });
-
-   request.fail(function( jqXHR, textStatus ) {
-     alert( "Request failed: " + textStatus );
-   });
-
-}*/
 function updateCoordinateStuff(row, col){
 
     //Update the view with the most recent coordinates
@@ -95,12 +46,10 @@ function updateCoordinateStuff(row, col){
     currentChosenRow = row;
     document.getElementById("chosenColSpan").innerHTML = col;
     currentChosenCol = col;
-    //console.log(currentChosenRow, currentChosenCol);
 
     //Create correct strings for URLs
     var fireString = "chosenFireFunc('/fire/" + row + "/" + col + "')";
     var scanString = "chosenScanFunc('/scan/" + row + "/" + col + "')";
-    //console.log(fireString, scanString);
 
     //Update onclicks
     document.getElementById('chosenFire').setAttribute('onclick', fireString);
@@ -164,29 +113,29 @@ function log(logContents){
 }
 
 function displayGameState(gameModel){
-$( '#MyBoard td'  ).css("background-color", "blue");
-$( '#TheirBoard td'  ).css("background-color", "blue");
+    $( '#MyBoard td'  ).css("background-color", "blue");
+    $( '#TheirBoard td'  ).css("background-color", "blue");
 
-displayShip(gameModel.aircraftCarrier);
-displayShip(gameModel.battleship);
-displayShip(gameModel.cruiser);
-displayShip(gameModel.destroyer);
-displayShip(gameModel.submarine);
+    displayShip(gameModel.aircraftCarrier);
+    displayShip(gameModel.battleship);
+    displayShip(gameModel.cruiser);
+    displayShip(gameModel.destroyer);
+    displayShip(gameModel.submarine);
 
-for (var i = 0; i < gameModel.computerMisses.length; i++) {
-   $( '#TheirBoard #' + gameModel.computerMisses[i].Across + '_' + gameModel.computerMisses[i].Down ).css("background-color", "green");
-}
-for (var i = 0; i < gameModel.computerHits.length; i++) {
-   $( '#TheirBoard #' + gameModel.computerHits[i].Across + '_' + gameModel.computerHits[i].Down ).css("background-color", "red");
-}
+    for (var i = 0; i < gameModel.computerMisses.length; i++) {
+       $( '#TheirBoard #' + gameModel.computerMisses[i].Across + '_' + gameModel.computerMisses[i].Down ).css("background-color", "green");
+    }
+    for (var i = 0; i < gameModel.computerHits.length; i++) {
+       $( '#TheirBoard #' + gameModel.computerHits[i].Across + '_' + gameModel.computerHits[i].Down ).css("background-color", "red");
+    }
+    for (var i = 0; i < gameModel.playerMisses.length; i++) {
+       $( '#MyBoard #' + gameModel.playerMisses[i].Across + '_' + gameModel.playerMisses[i].Down ).css("background-color", "green");
+    }
+    for (var i = 0; i < gameModel.playerHits.length; i++) {
+       $( '#MyBoard #' + gameModel.playerHits[i].Across + '_' + gameModel.playerHits[i].Down ).css("background-color", "red");
+    }
 
-for (var i = 0; i < gameModel.playerMisses.length; i++) {
-   $( '#MyBoard #' + gameModel.playerMisses[i].Across + '_' + gameModel.playerMisses[i].Down ).css("background-color", "green");
-}
-for (var i = 0; i < gameModel.playerHits.length; i++) {
-   $( '#MyBoard #' + gameModel.playerHits[i].Across + '_' + gameModel.playerHits[i].Down ).css("background-color", "red");
-}
-
+    // Added this flag check to make the scan alert only fire on scanning
     if (scanFlag) {
         //Check to see if the scan found anything and display
         if(gameModel.scanResult){
